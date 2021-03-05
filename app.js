4d852f5be5bb364846d252864d9c3cd6b0f2a204
AnjaliDeshwani <deshwanianjali@gmail.com>
2021-03-05 16:27:11 +0530

    refactor: replaced the mock URL

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text){
     return serverURL + "?text=" +text;
